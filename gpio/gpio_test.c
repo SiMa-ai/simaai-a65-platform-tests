fa3b70328bd8156f3c34009a9e59c59812110840
Nilesh Raghuvanshi <nilesh.r@sima.ai>
2022-08-12 10:19:25 +0530

    GPIO: Added test cases to set individual GPIOs
    Signed-off-by: Nilesh Raghuvanshi <nilesh.r@sima.ai>

--- gpio/gpio_test.c
+++ gpio/gpio_test.c
@@ -263,14 +263,169 @@ void overnight_test() {
 	}
 }
 
+void get_chip_and_gpio_number(unsigned int *chip_number,
+				unsigned int *gpio_number) {
+
+	if((!chip_number) || (!gpio_number)) {
+		printf("ERROR : invalid input parameters\n");
+		return;
+	}
+
+	while(1) {
+		printf("Enter the chip or port number\n");
+		if(scanf("%u",chip_number) == 1) {
+			if(((*chip_number) < 0) || ((*chip_number) > 3)) {
+				printf("ERROR : invalid chip number, valid range 0 to 3\n");
+				continue;
+			}
+			printf("Enter the gpio number for chip %u\n",*chip_number);
+			if(scanf("%u", gpio_number) == 1) {
+				if(((*gpio_number) < 0) || ((*gpio_number) > 7)) {
+					printf("ERROR : invalid port number, valid range 0 to 7\n");
+					continue;
+				}
+				//printf("Reading port %u from chip %u\n", *gpio_number,
+				//					*chip_number);
+				break;
+			} else {
+				printf("invalid gpio number\n");
+				continue;
+			}
+		} else {
+			printf("invalid chip number\n");
+			continue;
+		}
+	} //while(1) ends
+}
+
+void read_gpio_port() {
+
+	struct gpiohandle_request req;
+	struct gpiohandle_data data;
+	int fd, rv;
+	unsigned int chip_number = -1;
+	unsigned int gpio_number = -1;
+	unsigned int read_val = -1;
+
+	get_chip_and_gpio_number(&chip_number, &gpio_number);
+	printf("INFO : reading gpio %u from chip %u\n", gpio_number, chip_number);
+
+	fd = open(port_paths[chip_number], O_RDWR);
+	if(fd == -1) {
+		printf("ERROR : opening port %s, errno:%d\n",port_paths[chip_number],
+						errno);
+		return;
+	}
+
+	req.lineoffsets[0] = gpio_number;
+	req.default_values[0] = 0;
+
+	req.lines = 1;
+	req.flags = GPIOHANDLE_REQUEST_INPUT;
+	strncpy(req.consumer_label,"GPIO_READ_PORT",15);
+
+	rv = ioctl(fd, GPIO_GET_LINEHANDLE_IOCTL, &req);
+	if(rv == -1) {
+		printf("ERROR : ioctl GPIO_GET_LINEHANDLE_IOCTL failed errno:%d\n",errno);
+		return;
+	}
+
+	rv = ioctl(req.fd, GPIOHANDLE_GET_LINE_VALUES_IOCTL, &data);
+	if(rv == -1) {
+		printf("ERROR : ioctl GPIOHANDLE_GET_LINE_VALUES_IOCTL failed errno:%d\n",errno);
+		return;
+	}
+
+	printf("INFO : value is %u\n",data.values[0]);
+	close(req.fd);
+	close(fd);
+}
+
+void write_gpio_port() {
+
+	struct gpiohandle_request req;
+	struct gpiohandle_data data;
+	int fd, rv;
+	unsigned int chip_number = -1;
+	unsigned int gpio_number = -1;
+	unsigned int val;
+
+	get_chip_and_gpio_number(&chip_number, &gpio_number);
+	printf("Enter the value to write, valid values 0 or 1\n");
+	if(scanf("%u",&val) == 1) {
+		printf("INFO : writing %u to gpio %u from chip %u\n", val, gpio_number,
+						chip_number);
+	} else {
+		printf("ERROR : invalid value try again!!!\n");
+		return;
+	}
+
+	fd = open(port_paths[chip_number], O_RDWR);
+	if(fd == -1) {
+		printf("ERROR : opening port %s, errno:%d\n",port_paths[chip_number],
+						errno);
+		return;
+	}
+
+	req.lineoffsets[0] = gpio_number;
+	req.default_values[0] = 0;
+
+	req.lines = 1;
+	req.flags = GPIOHANDLE_REQUEST_OUTPUT;
+	strncpy(req.consumer_label,"GPIO_WRITE_PORT",16);
+
+	rv = ioctl(fd, GPIO_GET_LINEHANDLE_IOCTL, &req);
+	if(rv == -1) {
+		printf("ERROR : ioctl GPIO_GET_LINEHANDLE_IOCTL failed errno:%d\n",errno);
+		return;
+	}
+
+	data.values[0] = val;
+	rv = ioctl(req.fd, GPIOHANDLE_SET_LINE_VALUES_IOCTL, &data);
+	if(rv == -1) {
+		printf("ERROR : ioctl GPIOHANDLE_GET_LINE_VALUES_IOCTL failed errno:%d\n",errno);
+		return;
+	}
+
+	printf("SUCCESS : set %u to gpio %u\n", val, gpio_number);
+
+	close(req.fd);
+	close(fd);
+}
+
+void pull_up_gpio_port () {
+
+	unsigned int chip_number = -1;
+	unsigned int gpio_number = -1;
+
+	get_chip_and_gpio_number(&chip_number, &gpio_number);
+	printf("INFO : Pulling up port %u from chip %u\n", gpio_number, chip_number);
+
+
+}
+
+void pull_down_gpio_port () {
+
+	unsigned int chip_number = -1;
+	unsigned int gpio_number = -1;
+
+	get_chip_and_gpio_number(&chip_number, &gpio_number);
+	printf("INFO : Pulling Down port %u from chip %u\n", gpio_number, chip_number);
+
+}
+
 void print_menu() {
 
 	printf("\t ===== select one of the below option =====\n");
 	printf("\t 1. Print port info \n");
 	printf("\t 2. Toggle all leds \n");
-	printf("\t 3. Print Binary\n");
+	printf("\t 3. Print Binary on leds\n");
 	printf("\t 4. Overnight Test \n");
-	printf("\t 5. Toggle all GPIOs\n");
+	printf("\t 5. Read from GPIO PORT\n");
+	printf("\t 6. Write to GPIO PORT\n");
+	printf("\t 7. Pull up GPIO PORT\n");
+	printf("\t 8. Pull down GPIO PORT\n");
+	printf("\t 9. Toggle all GPIOs\n");
 	printf("\t 0. Quit\n");
 }
 
@@ -314,6 +469,18 @@ int main() {
 						overnight_test();
 						break;
 					case 5:
+						read_gpio_port();
+						break;
+					case 6:
+						write_gpio_port();
+						break;
+					case 7:
+						pull_up_gpio_port();
+						break;
+					case 8:
+						pull_down_gpio_port();
+						break;
+					case 9:
 						printf("Toggling all GPIOs\n");
 						toggle_all_gpios(10);
 						break;
